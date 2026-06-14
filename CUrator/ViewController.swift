0c1825c5249547bca7caa83de6677fc3463a2b77
ys1217-gg <68524737+ys1217-gg@users.noreply.github.com>
2026-06-14 14:27:02 +0900

    Initial Commit

---
+++ CUrator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CUrator
+//
+//  Created by 황요성 on 6/14/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
